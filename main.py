6169260e02680f211f01e47aa08c1e3728e87b28
Tonaion02 <antonioambrosiopub@gmail.com>
2024-08-20 14:12:46 +0200

    PARSER: successfully retrieving and creation of directories' structure

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ import xml.etree.ElementTree as ET
 # Builds and contains all the paths necessary for the program
 # TODO: necessary to understand what to do where i am not in debug mode with the BASE_DIR
 # what directory become the BASE_DIR?
+# TODO: necessary rebase all the paths from a full path
 # ------------------------------------------------------------------------------------------------------------------
 class DIRECTORIES_NAME:
 
@@ -34,9 +35,9 @@ class DIRECTORIES_NAME:
 
 
     @staticmethod
-    def _init():
+    def _init(path: str):
         if DIRECTORIES_NAME._debug:
-            DIRECTORIES_NAME._BASE_DIR = DIRECTORIES_NAME._GARBAGE_DIR
+            DIRECTORIES_NAME._BASE_DIR = os.path.join(path, DIRECTORIES_NAME._GARBAGE_DIR)
         else:
             pass # SYSTEM_BASE_DIR
 
@@ -44,6 +45,10 @@ class DIRECTORIES_NAME:
     def _get_garbage_dir():
         return DIRECTORIES_NAME._GARBAGE_DIR
     
+    @staticmethod
+    def _get_base_dir():
+        return DIRECTORIES_NAME._BASE_DIR
+
     @staticmethod
     def _get_downloads_dir():
         return os.path.join(DIRECTORIES_NAME._BASE_DIR, DIRECTORIES_NAME._DOWNLOADS_DIR)
@@ -55,10 +60,10 @@ class DIRECTORIES_NAME:
 
 
 # Don't touch this, PRIVATE (START)
-DIRECTORIES_NAME._init()
+DIRECTORIES_NAME._init("C:/source/Python/windows-configurator")
 GARBAGE_DIR = DIRECTORIES_NAME._get_garbage_dir()
 DOWNLOADS_DIR = DIRECTORIES_NAME._get_downloads_dir()
-BASE_DIR = DIRECTORIES_NAME._get_downloads_dir()
+BASE_DIR = DIRECTORIES_NAME._get_base_dir()
 TO_MANUALLY_INSTALL_DIR = DIRECTORIES_NAME._get_to_manually_install_dir()
 # Don't touch this, PRIVATE (END)
 #===================================================================================================================
@@ -77,10 +82,13 @@ class OUTPUT_FORMATTER:
     @staticmethod
     def print_logo():
         print("+===================================================================================================================+")
-        print("+                                          WINDOWS-CONFIGURATOR                                                     +")
+        print("|                                          WINDOWS-CONFIGURATOR                                                     |")
         print("+===================================================================================================================+")
 
 #===================================================================================================================
+
+
+
 
 
 def install_software(url: str, outfile, installation_directory=None):
@@ -126,6 +134,7 @@ def setX(name: str, value: str) -> None:
 
 
 
+
 # TEMP
 def testParseXml(name: str) -> None:
 
@@ -136,56 +145,107 @@ def testParseXml(name: str) -> None:
         def __str__(self):
             return "BackupPath: " + str(self.intern)
         
+    class TAGS:
+        DIRECTORY = "directory"
+        FILE = "file"
+        SOFTWARE = "software"
+        DATA = "data"
+        CHOCO = "chocolatey-dependencies"
+    
+    class ATTRIB:
+        NAME = "name"
+
+
     def print_element(e):
-        if type(e) is BackupPath:
+        if isinstance(e, Path): 
             print(str(e))
             
         elif type(e) is ET.Element:
             print("Element: " + e.tag + " name: " + str(e.attrib) + " childs number: " + str(len(list(e))))
 
+
+
     # Variable that mantain the path of the current working directory like a string
-    actualPath = BASE_DIR
+    cwd_path = BASE_DIR
 
     # Starting to parse file and retrieve root element
     tree = ET.parse(name)
     root = tree.getroot()
 
     # Use list like a stack with append and pop
-    stackElements = []
-    stackElements.append(root)
+    # Warning: we append and pop from the end of
+    # the stack
+    stack_elements = []
+    stack_elements.append(root)
 
-    while len(stackElements) > 0:
+    # Change current working directory
+    os.chdir(cwd_path)
+
+
+
+    while len(stack_elements) > 0:
 
         # DEBUG
         print("\n StackElements:")
-        for element in stackElements:
+        for element in stack_elements:
             print_element(element)
         print("StackElements end")
         
         # Pop the last element
-        poppedElement = stackElements.pop(len(stackElements) - 1)
+        popped_element = stack_elements.pop(len(stack_elements) - 1)
 
         # Check if the last element is a BackupPath or an Element of the tree (START)
-        if type(poppedElement) is BackupPath:
+        if isinstance(popped_element, BackupPath):
             # DEBUG
-            print(poppedElement)
+            print(popped_element)
 
-            # TODO change effectevly the current working directory
-            actualPath = poppedElement.intern
+            # TODO change effectevly the current working directory only if the current working directory
+            # is different from the directory where we must be
+            # TEMP(temporary solution that doesn't work properly)
+            if cwd_path != popped_element.intern:
+                cwd_path = popped_element.intern
+                os.chdir(cwd_path)
             continue
-        elif type(poppedElement) is ET.Element:
-            pass
+
+        elif type(popped_element) is ET.Element:
+            if popped_element.tag == TAGS.DIRECTORY:
+                # If the directory "cwd_path\popped_element.attrib['name']" doesn't exist create it
+                popped_element_dir_path = os.path.join(cwd_path, popped_element.attrib[ATTRIB.NAME])
+                if not os.path.isdir(popped_element_dir_path):
+                    os.mkdir(popped_element_dir_path)
+
+                # if the directory isn't empty and we are in another directory, change directory
+                # then save the current value of cwd_path in a BackupPath in a 
+                if len(list(popped_element)) > 0:
+                    stack_elements.append(BackupPath(cwd_path))
+                    cwd_path = popped_element_dir_path
+                    os.chdir(cwd_path)
+
+
+
+            elif popped_element.tag == TAGS.FILE:
+                pass
+            elif popped_element.tag == TAGS.SOFTWARE:
+                pass
+            elif popped_element.tag == TAGS.DATA:
+                pass
+            elif popped_element.tag == TAGS.CHOCO:
+                pass
+            else :
+                # DEBUG
+                print("WARNING! tag that i doesn't know")
         # Check if the last element is a BackupPath or an Element of the tree (END)
 
-        if len(list(poppedElement)) > 0:
-            stackElements.append(BackupPath(actualPath))
 
-        # Retrieve all the child elements of the poppedElement and put in the stack
-        for child in poppedElement:
-            stackElements.append(child)
+        
+
+
+        # Retrieve all the child elements of the popped_element and put in the stack
+        for child in popped_element:
+            stack_elements.append(child)
 
         # DEBUG
-        print_element(poppedElement)
+        print_element(popped_element)
 
         
 
@@ -204,6 +264,8 @@ if __name__ == "__main__":
 
     # TEMP
     os.chdir("C:/source/Python/windows-configurator")
+
+    print()
 
     # Cleaning
     shutil.rmtree("garbage", ignore_errors=True)
