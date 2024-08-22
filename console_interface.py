c81aab3d4caa34d1b7f62c7d85ba34abcf99b190
Tonaion02 <antonioambrosiopub@gmail.com>
2024-08-22 08:45:33 +0200

    CURSOR POSITION: fixed a problem and added some utility to move cursor easily

--- console_interface.py
+++ console_interface.py
@@ -9,25 +9,35 @@ import os
 # CONSOLE_INTERFACE Class
 # This is a full static Class
 # This class contains methods(static) to print on the command line output for UI/debug purpose
+# ANSI escape for move cursor:https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
 #-------------------------------------------------------------------------------------------------------------------
 class CONSOLE_INTERFACE:
 
     END_OF_LOGO = (0, 0)
 
+
+
+    # Back home with cursor (0, 0)
     @staticmethod
-    def print_logo():
+    def __home_cursor_ansi() -> str:
+        return '\x1b[H'
+    
+    # Set cursor position
+    @staticmethod
+    def __setc_ansi(t: tuple) -> str:
+        return '\x1b[{};{}f'.format(t[0], t[1])
+
+    @staticmethod
+    def print_logo() -> None:
         os.system('cls')
 
         print(CONSOLE_COLORS.CYAN + "+===================================================================================================================+")
         print("|                                          WINDOWS-CONFIGURATOR                                                     |")
         print("+===================================================================================================================+" + CONSOLE_COLORS.WHITE)
 
-        CONSOLE_INTERFACE.END_OF_LOGO = (0, 0) 
-        print("===================================================================")
-        
-        # Move to the start of the console with ansi escape
-        # https://stackoverflow.com/questions/40103919/move-console-cursor-up
-        print('\033[1;1H')
+        CONSOLE_INTERFACE.END_OF_LOGO = (3, 1) 
+        print("===================================================================" + CONSOLE_INTERFACE.__setc_ansi((1, 1)) + "Hello")
+        print(CONSOLE_INTERFACE.__setc_ansi((5, 1)))
 
 #===================================================================================================================
 
